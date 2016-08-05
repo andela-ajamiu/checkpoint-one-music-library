f99fe94deaf17e7bd4c457cf0beb740930925f25
Abdulmujeeb Jamiu <abdulmujeeb.jamiu@andela.com>
2016-08-05 15:18:15 +0100

    fix: codeclimate code duplicate issue

---
+++ lib/message.rb
@@ -0,0 +1,28 @@
+class Info
+
+  def self.greetings
+    puts <<-GREETINGS.cyan
+      =============================================================
+                        Welcome to Ruby Music Library
+      =============================================================
+      GREETINGS
+  end
+
+
+  def self.help
+    puts <<-HELP.green
+      =============================================================
+                          List of available commands
+      =============================================================
+      \"list songs\" - To display all the Songs available in the library
+      \"list artists\" - To display all Artists with one or more songs
+      \"list genres\" - To display all the Genres of the songs
+      \"play song\" - To play a specific Song with song number
+      \"list artist\" - To display all Songs that belongs to an artist
+      \"list genre\" - To display all songs that belongs to a genre
+      \"exit\" - To exit the application
+      HELP
+    puts "Please enter any of the commands above....".cyan
+  end
+
+end
